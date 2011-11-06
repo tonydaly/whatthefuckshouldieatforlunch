641b7d9507b2bb7de5caca7e2bab73b21c5e93ca
Tony Daly <tony.daly@me.com>
2011-11-06 19:08:06 +0000

    Randomised order

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,7 +2,9 @@ class RootController < ApplicationController
   respond_to :json, :html
 
   def index
-    @businesses = $yelp.find(category_filter: "restaurants", location: "london").businesses
-    respond_with(@businesses)  
+    @businesses = $yelp.find(category_filter: "restaurants", location: "london").businesses.shuffle
+    respond_with(@businesses) do |format|
+      format.html { @business = @businesses.first }
+    end
   end
 end
